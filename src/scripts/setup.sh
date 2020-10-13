4f6bf62b0bb7556fd1730ed7eaf7e54336e27a80
akopylov <kopylov.anton@gmail.com>
2020-10-13 17:15:10 +0300

    build & setup fix

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -11,17 +11,17 @@ source /home/flask/api/python-api-env/bin/activate
 cd /home/flask/api/$serviceName
 pwd
 cwd=$(pwd)
-python3 $cwd/scripts/template/setup_new_service.py --newName $serviceName --tempName "+-template-+" || { echo '!!!Finish with ERROR: checks and rename failed' ; exit 1; }
+python3 $cwd/template/setup_new_service.py --newName $serviceName --tempName "+-template-+" || { echo '!!!Finish with ERROR: checks and rename failed' ; exit 1; }
 mkdir -p /home/flask/api/socket
 mkdir -p /home/flask/api/nginx
 echo "{'rc': 500, 'message': 'Internal server error'}" > /home/flask/api/nginx/500.json
 echo "{'rc': 404, 'message': 'Can not find resources'}" > /home/flask/api/nginx/404.json
 chown -R flask:nginx /home/flask/api/
 chmod +x run.sh
-cp ./scripts/template/systemd/api-template.service ./template/systemd/api-$serviceName.service || { echo '!!!Finish with ERROR: service move failed' ; exit 1; }
-cp ./scripts/template/systemd/api-template.socket ./template/systemd/api-$serviceName.socket || { echo '!!!Finish with ERROR: socket move failed' ; exit 1; }
-cp ./scripts/template/systemd/api-$serviceName.service /etc/systemd/system || { echo '!!!Finish with ERROR: service copy failed' ; exit 1; }
-cp ./scripts/template/systemd/api-$serviceName.socket /etc/systemd/system || { echo '!!!Finish with ERROR: socket move failed' ; exit 1; }
+cp ./template/systemd/api-template.service ./template/systemd/api-$serviceName.service || { echo '!!!Finish with ERROR: service move failed' ; exit 1; }
+cp ./template/systemd/api-template.socket ./template/systemd/api-$serviceName.socket || { echo '!!!Finish with ERROR: socket move failed' ; exit 1; }
+cp ./template/systemd/api-$serviceName.service /etc/systemd/system || { echo '!!!Finish with ERROR: service copy failed' ; exit 1; }
+cp ./template/systemd/api-$serviceName.socket /etc/systemd/system || { echo '!!!Finish with ERROR: socket move failed' ; exit 1; }
 chmod 755 /etc/systemd/system/api-$serviceName.service
 chmod 755 /etc/systemd/system/api-$serviceName.socket
 systemctl daemon-reload || { echo '!!!Finish with ERROR: systemd comand failed' ; exit 1; }
