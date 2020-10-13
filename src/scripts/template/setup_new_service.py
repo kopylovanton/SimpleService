2ef89f9d88d66827c03bfe7149a06e71daf079ea
akopylov <kopylov.anton@gmail.com>
2020-10-13 17:07:16 +0300

    build & setup fix

--- src/scripts/template/setup_new_service.py
+++ src/scripts/template/setup_new_service.py
@@ -3,7 +3,7 @@ import os
 import shutil
 
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--newName', help='new service name')
+parser.add_argument('--newName', help='new service name', default='simpleservice')
 parser.add_argument('--tempName', help='new service name', default='+-template-+')
 args = parser.parse_args()
 print('get args:', args)
@@ -13,13 +13,19 @@ assert dirpath.find(args.newName) > 0, 'Current dir does not equal service name 
 
 
 def doChange(p):
-    sp = ['.py', '.temp', '.log', 'cache', '.git', '.idea', '.md','.pyc','.zip','.tar']
-    if p.find('/home/flask') < 0:
-        return False
+    sp = ['run.sh',
+          'config_log.yaml',
+          'swagger_template.yaml',
+          'config_service.yaml',
+          'systemd/api-template.socket',
+          'systemd/api-template.service',
+          'nginx/api-config']
+    if p.find(args.newName) < 0:
+         return False
     for v in sp:
         if p.find(v) > 0:
-            return False
-    return True
+            return True
+    return False
 
 
 cntRep = 0
@@ -29,9 +35,10 @@ for dname, dirs, files in os.walk(dirpath):
         if doChange(fpath):
             with open(fpath) as f:
                 s = f.read()
-            s = s.replace(args.tempName, args.newName)
-            cntRep += s.count(args.newName)
-            print('>>> prcess file:', fpath, 'replaced %s template' % str(s.count(args.newName)))
+            cntRep += s.count(args.tempName)
+            if cntRep>0:
+                s = s.replace(args.tempName, args.newName)
+            print('>>> process file:', fpath, 'replaced %s template' % str(cntRep))
             with open(fpath, "w") as f:
                 f.write(s)
 assert cntRep > 3, 'Error replace template service name - try copy new template configuration' \
@@ -49,9 +56,9 @@ addconf = '''location /%s {
       proxy_pass http://unix:/home/flask/api/socket/%s.sock;
     } 
     location /''' % (args.newName, args.newName)
-if os.path.exists('/etc/nginx/sites-available/api-config'):
-    if os.path.isfile('/etc/nginx/sites-available/api-config'):
-        shutil.copyfile('/etc/nginx/sites-available/api-config', dirpath + '/nginx/api-config')
+if  os.path.exists('/etc/nginx/sites-available/api-config') \
+    and os.path.isfile('/etc/nginx/sites-available/api-config'):
+        shutil.copyfile('/etc/nginx/sites-available/api-config', dirpath + 'template/nginx/api-config')
         fpath = dirpath + '/nginx/api-config'
         with open(fpath) as f:
             s = f.read()
@@ -59,3 +66,4 @@ if os.path.exists('/etc/nginx/sites-available/api-config'):
             s = s.replace('location /', addconf, 1)
             with open(fpath, "w") as f:
                 f.write(s)
+
